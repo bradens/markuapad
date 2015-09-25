a1e8c8dad52de519c3aa23fd4c764d318f7e066d
Braden Simpson <braden@uvic.ca>
2015-09-25 15:59:25 -0700

    Added better usage of the api calls to the ace editor

--- src/jsx/editor.jsx
+++ src/jsx/editor.jsx
@@ -20,7 +20,7 @@ class Editor extends React.Component {
 
   // Setup all the editor options when we mount.
   componentDidMount() {
-    this.setupEditor();
+    this.loadFile();
   }
 
   componentWillUpdate(nextProps, nextState) {
@@ -37,16 +37,20 @@ class Editor extends React.Component {
   componentDidUpdate(lastProps, lastState) {
     if (lastProps.currentFile !== this.props.currentFile) {
       this.setState({ currentFileValue: null })
-      this.setupEditor();
+      this.loadFile();
     }
   }
 
-  setupEditor() {
+  loadFile() {
     if (!this.props.currentFile) return;
 
-    // Setup the ace editor
+    // Get the current file to put in the ace editor
+    FileAccessor.get(this.props.currentFile.filename, this.onCurrentFileLoaded, this.props.currentFile.type);
+  }
+
+  setupEditor() {
+    // Set all editor related properties here.
     this.editor = ace.edit(this.refs.editor.getDOMNode());
-    this.editor.getSession().setMode('ace/mode/markdown');
     this.editor.setTheme('ace/theme/xcode');
     this.editor.setKeyboardHandler('ace/keyboard/emacs');
     this.editor.renderer.setShowGutter(false);
@@ -55,15 +59,8 @@ class Editor extends React.Component {
     this.editor.setShowPrintMargin(false);
     this.editor.setFontSize(14);
     this.editor.$blockScrolling = Infinity;
-    this.editor.session.setUseWrapMode(true);
     this.editor.setHighlightActiveLine(false);
-
-    // When the editor changes, alert us
     this.editor.on("change", this.onEditorChanged)
-    this.editor.getSession().selection.on("changeCursor", this.onCursorChanged)
-
-    // Get the current file to put in the ace editor
-    FileAccessor.get(this.props.currentFile.filename, this.onCurrentFileLoaded, this.props.currentFile.type);
   }
 
   // When someone changes the cursor, we need to tell the file that we have a changed cursor
@@ -99,12 +96,27 @@ class Editor extends React.Component {
     if (error)
       return console.error(error)
     else {
-      // Focus the editor
-      this.editor.focus;
-
       // Set the value of it
-      this.setState({ currentFileValue: contents }, () => this.editor.setValue(contents, -1));
+      this.setState({ currentFileValue: contents }, this.setupCurrentFile);
     }
+  }
+
+  // Called when we load a new file.  Sets up the files editor session, and initial values.
+  // TODO we should actually probably keep a set of edit sessions in our state, and then switch between them to keep the
+  setupCurrentFile = () => {
+    this.setupEditor();
+
+    let newSession = new ace.EditSession(this.state.currentFileValue);
+    // Add some session modes
+    // TODO parse file type here and change mode and store session info
+    newSession.setMode('ace/mode/markdown');
+    newSession.setUseWrapMode(true);
+    newSession.selection.on("changeCursor", this.onCursorChanged)
+    newSession.setUndoManager(new ace.UndoManager());
+
+    // Make the editor load this session
+    this.editor.setSession(newSession)
+    this.editor.focus()
   }
 
   renderEditor() {
