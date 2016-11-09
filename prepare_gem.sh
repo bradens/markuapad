2399c2aa6fd1b323e2d54daa89432350ec13e0a0
Braden Simpson <braden@uvic.ca>
2016-11-09 14:16:12 -0700

    Add the bulid step to prepare_gem.sh

--- prepare_gem.sh
+++ prepare_gem.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+
+PROD=true ./node_modules/.bin/webpack --config webpack.config.js
+
 cp build/dist.css app/assets/stylesheets/markuapad.css
 cp build/dist.js app/assets/javascripts/markuapad.js
+
+
