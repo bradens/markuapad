a3d8e8cd0bcfb6b80ef2265ae492135b26d73988
Braden Simpson <braden@uvic.ca>
2016-11-10 08:57:54 -0700

    Don't bother with uglification

--- prepare_gem.sh
+++ prepare_gem.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-PROD=true ./node_modules/.bin/webpack --config webpack.config.js
+./node_modules/.bin/webpack --config webpack.config.js
 
 cp build/dist.css app/assets/stylesheets/markuapad.css
 cp build/dist.js app/assets/javascripts/markuapad.js
